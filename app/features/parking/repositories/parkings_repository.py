3c7c6138cb414ad83c736d306fe3c66b2f993a97
DevJuan001 <juanesyt7@gmail.com>
2026-08-08 19:29:43 -0500

    feat(parking): implement find_parking_by_id method with error handling and response mapping

--- app/features/parking/repositories/parkings_repository.py
+++ app/features/parking/repositories/parkings_repository.py
@@ -1,3 +1,4 @@
+from app.features.parking.models.parking_responses import ParkingResponse
 from app.utils.logger import get_logger
 from app.features.parking.models.parking_schemas import UpdateParkingSchema
 
@@ -5,6 +6,39 @@ logger = get_logger("parkings.repository")
 
 
 class ParkingsRepository:
+
+    @staticmethod
+    def find_parking_by_id(parking_id: int, connection):
+        cursor = connection.cursor()
+
+        query = """
+        SELECT
+            p.id,
+            p.name,
+            c.name
+        FROM PARKINGS AS p
+        INNER JOIN COUNTRIES AS c
+            ON p.country_id = c.id
+        WHERE p.id = %s
+        """
+
+        try:
+            cursor.execute(query, (parking_id,))
+
+            result = cursor.fetchone()
+
+            return None, ParkingResponse(
+                id=result[0],
+                name=result[1],
+                country=result[2]
+            )
+
+        except Exception as e:
+            logger.error("Error en find_parking_by_id: %s", e, exc_info=True)
+            return "Error al intentar obtener la información del parking", None
+
+        finally:
+            cursor.close()
 
     @staticmethod
     def create_parking(name: str, country_id: int, connection):
