a06d1c6ef374f01fb62e489807fd7f85f19d9971
DevJuan001 <juanesyt7@gmail.com>
2026-08-10 17:22:56 -0500

    fix(parking): update parking_id type from int to str in ParkingService methods for consistency and add UUID generation for new parking entries

--- app/features/parking/services/parking_service.py
+++ app/features/parking/services/parking_service.py
@@ -1,20 +1,21 @@
 from app.utils.logger import get_logger
+from app.utils.uuid import generate_uuid
 from app.core.exception import ServiceError
 from app.core.database import get_connection
 from app.utils.plate_formatter import plate_formatter
-from app.features.parking.models.parking_schemas import CreatePlateSchema, UpdateParkingSchema
-from app.features.parking.repositories.parkings_repository import ParkingsRepository
-from app.features.parking.repositories.plates_repository import PlatesRepository
-from app.features.parking.repositories.vehicle_types_repository import VehicleTypesRepository
 from app.features.spots.models.spots_schemas import SpotsFiltersSchema
 from app.features.spots.repositories.spots_repository import SpotsRepository
+from app.features.parking.repositories.plates_repository import PlatesRepository
+from app.features.parking.repositories.parkings_repository import ParkingsRepository
+from app.features.parking.models.parking_schemas import CreatePlateSchema, UpdateParkingSchema
+from app.features.parking.repositories.vehicle_types_repository import VehicleTypesRepository
 
 logger = get_logger("parking.service")
 
 
 class ParkingService:
     @staticmethod
-    def get_parking_by_id(parking_id: int):
+    def get_parking_by_id(parking_id: str):
         connection = get_connection()
 
         try:
@@ -58,7 +59,10 @@ class ParkingService:
                     "El pais del parking es obligatorio, selecciona uno e intentalo nuevamente"
                 )
 
+            uuid = generate_uuid()
+
             error, success, parking_id = ParkingsRepository.create_parking(
+                uuid=uuid,
                 name=name.strip(),
                 country_id=country_id,
                 connection=connection
@@ -88,7 +92,7 @@ class ParkingService:
             connection.close()
 
     @staticmethod
-    def get_all_plates(parking_id: int):
+    def get_all_plates(parking_id: str):
         connection = get_connection()
 
         try:
@@ -144,7 +148,7 @@ class ParkingService:
             connection.close()
 
     @staticmethod
-    def get_all_spots(parking_id: int, filters: SpotsFiltersSchema):
+    def get_all_spots(parking_id: str, filters: SpotsFiltersSchema):
         connection = get_connection()
 
         try:
@@ -172,7 +176,7 @@ class ParkingService:
             connection.close()
 
     @staticmethod
-    def get_plate_by_name(parking_id: int, plate: str):
+    def get_plate_by_name(parking_id: str, plate: str):
         connection = get_connection()
 
         try:
@@ -200,7 +204,7 @@ class ParkingService:
             connection.close()
 
     @staticmethod
-    async def create_plate(parking_id: int, plate_data: CreatePlateSchema):
+    async def create_plate(parking_id: str, plate_data: CreatePlateSchema):
         connection = get_connection()
 
         try:
@@ -264,7 +268,7 @@ class ParkingService:
             connection.close()
 
     @staticmethod
-    def update_parking(parking_id: int, parking_data: UpdateParkingSchema):
+    def update_parking(parking_id: str, parking_data: UpdateParkingSchema):
         connection = get_connection()
 
         try:
