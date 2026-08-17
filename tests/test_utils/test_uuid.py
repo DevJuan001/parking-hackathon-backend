b0ea604abf32d8972c550465ee993d09d553e3e9
DevJuan001 <juanesyt7@gmail.com>
2026-08-17 17:54:17 -0500

    test(uuid): add unit test for UUID generation to validate UUID v4 format

---
+++ tests/test_utils/test_uuid.py
@@ -0,0 +1,11 @@
+import re
+
+from app.utils.uuid import generate_uuid
+
+_UUID_V4 = re.compile(
+    r"^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+)
+
+
+def test_generate_uuid_matches_regex_v4():
+    assert _UUID_V4.match(generate_uuid())
