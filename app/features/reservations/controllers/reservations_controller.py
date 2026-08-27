0041ad10f0d0741014d29c0f445b4e1fa31b6301
DevJuan001 <juanesyt7@gmail.com>
2026-08-26 19:37:26 -0500

    refactor(reservations_controller): update payload type to AuthPayload for improved type safety and streamline return statements

--- app/features/reservations/controllers/reservations_controller.py
+++ app/features/reservations/controllers/reservations_controller.py
@@ -7,26 +7,27 @@ from app.features.reservations.models.reservations_schemas import (
     UpdateReservationSchema,
 )
 from app.features.reservations.services.reservations_service import ReservationsService
+from app.middlewares.jwt_middleware import AuthPayload
 
 
 class ReservationsController:
     @staticmethod
-    def get_all_reservations(filters: FilterReservationsSchema, payload: dict):
+    def get_all_reservations(filters: FilterReservationsSchema, payload: AuthPayload):
         error, reservations = ReservationsService.get_all_reservations(
-            filters, str(payload["parking_id"])
+            filters, payload.parking_id
         )
 
         if error:
             raise HTTPException(status_code=400, detail=error)
 
-        return {
-            "data": reservations
-        }
+        return {"data": reservations}
 
     @staticmethod
-    def create_reservation_for_user(data: CreateReservationSchema, payload: dict):
+    def create_reservation_for_user(
+        data: CreateReservationSchema, payload: AuthPayload
+    ):
         error, success, message = ReservationsService.create_reservation(
-            str(payload["parking_id"]),
+            payload.parking_id,
             data.name,
             data.plate,
             data.email,
@@ -40,10 +41,7 @@ class ReservationsController:
         if error:
             raise HTTPException(status_code=400, detail=error)
 
-        return {
-            "success": success,
-            "message": message
-        }
+        return {"success": success, "message": message}
 
     @staticmethod
     def create_reservation_for_self(data: CreateSelfReservationSchema):
@@ -62,35 +60,28 @@ class ReservationsController:
         if error:
             raise HTTPException(status_code=400, detail=error)
 
-        return {
-            "success": success,
-            "message": message
-        }
+        return {"success": success, "message": message}
 
     @staticmethod
-    def update_reservation(reservation_id: int, data: UpdateReservationSchema, payload: dict):
+    def update_reservation(
+        reservation_id: int, data: UpdateReservationSchema, payload: AuthPayload
+    ):
         error, success, message = ReservationsService.update_reservation(
-            reservation_id, data, str(payload["parking_id"])
+            reservation_id, data, payload.parking_id
         )
 
         if error:
             raise HTTPException(status_code=400, detail=error)
 
-        return {
-            "success": success,
-            "message": message
-        }
+        return {"success": success, "message": message}
 
     @staticmethod
-    def delete_reservation(reservation_id: int, payload: dict):
+    def delete_reservation(reservation_id: int, payload: AuthPayload):
         error, success, message = ReservationsService.delete_reservation(
-            reservation_id, str(payload["parking_id"])
+            reservation_id, payload.parking_id
         )
 
         if error:
             raise HTTPException(status_code=400, detail=error)
 
-        return {
-            "success": success,
-            "message": message
-        }
+        return {"success": success, "message": message}
